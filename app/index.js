b9f8f6dbf6033c6c7113710c1a3f26173493afdb
Jared Hanson <jaredhanson@gmail.com>
2017-03-27 11:17:43 -0700

    Rename folder.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 exports = module.exports = {
-  'response/permission': require('./xom/response/permission'),
+  'response/permission': require('./response/permission'),
 };
 
 exports.load = function(id) {
   try {
-    return require('./xom/' + id);
+    return require('./' + id);
   } catch (ex) {
     if (ex.code == 'MODULE_NOT_FOUND') { return; }
     throw ex;
