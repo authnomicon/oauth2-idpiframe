85e77bf59e4aac93619d3f0ef03161c1919aacc5
Jared Hanson <jaredhanson@gmail.com>
2017-04-17 11:11:21 -0700

    Configure querystring.

--- www/js/main.js
+++ www/js/main.js
@@ -1,14 +1,8 @@
-console.log('IFRAME MAIN!');
-
 require.config({
   baseUrl: 'js/lib',
-  paths: {
-    //'idpiframe': 'github/jaredhanson/idpiframe@0.0.0/main',
-    //'querystring': 'github/anchorjs/querystring@0.0.1/querystring',
-  },
   packages: [
     { name: 'idpiframe', location: 'idpiframe' },
-    //{ name: 'querystring', location: 'github/anchorjs/querystring@0.0.1', main: 'querystring' }
+    { name: 'querystring', location: 'querystring', main: 'querystring' }
   ]
 });
 
