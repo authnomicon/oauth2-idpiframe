69373924a9c227a9fe37b22b5fe2812004ed9eb4
Jared Hanson <jaredhanson@gmail.com>
2021-12-07 15:48:12 -0800

    Serve www files using static middleware.

--- com/iframe/http/service.js
+++ com/iframe/http/service.js
@@ -3,7 +3,9 @@ exports = module.exports = function() {
   var path = require('path');
   
   var router = new express.Router();
-  router.use(express.static(path.resolve(__dirname, 'public')));
+  //router.use(express.static(path.resolve(__dirname, 'public')));
+  router.use(express.static(path.resolve(__dirname, '../../../www')));
+  router.use('/js/lib', express.static(path.join(__dirname, '../../../www/node_modules')));
   
   return router;
 };
