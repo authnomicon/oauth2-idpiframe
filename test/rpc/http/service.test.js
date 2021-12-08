9f4a726ca06f45ae7b0c28a2785396f3cf0552ac
Jared Hanson <jaredhanson@gmail.com>
2021-12-07 21:21:09 -0800

    Add tests.

---
+++ test/rpc/http/service.test.js
@@ -0,0 +1,24 @@
+/* global describe, it */
+
+var expect = require('chai').expect;
+var factory = require('../../../com/rpc/http/service');
+
+
+describe('rpc/http/service', function() {
+  
+  it('should be annotated', function() {
+    expect(factory['@implements']).to.equal('http://i.bixbyjs.org/http/Service');
+    expect(factory['@path']).to.equal('/oauth2/iframerpc');
+    expect(factory['@singleton']).to.be.undefined;
+  });
+  
+  it('should construct service', function() {
+    function rpcHandler() {};
+    
+    var service = factory(rpcHandler);
+    
+    expect(service).to.be.a('function');
+    expect(service.length).to.equal(3);
+  });
+  
+});
