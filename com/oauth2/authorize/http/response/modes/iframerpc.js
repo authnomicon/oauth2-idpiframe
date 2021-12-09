f9968fc3184827a860a4896caa833f526b202da8
Jared Hanson <jaredhanson@gmail.com>
2021-12-08 16:39:39 -0800

    Expose the internal IFrame RPC response mode.

---
+++ com/oauth2/authorize/http/response/modes/iframerpc.js
@@ -0,0 +1,6 @@
+exports = module.exports = function() {
+  return require('oauth2orize-iframerpcrm');
+};
+
+exports['@implements'] = 'http://i.authnomicon.org/oauth2/authorization/http/ResponseMode';
+exports['@mode'] = '.iframerpc';
