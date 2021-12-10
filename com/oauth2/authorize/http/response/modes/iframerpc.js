3a732688639711dae7561516bc5d6c4d02eeaa89
Jared Hanson <jaredhanson@gmail.com>
2021-12-09 16:34:51 -0800

    Extend issueToken response with session_state.

--- com/oauth2/authorize/http/response/modes/iframerpc.js
+++ com/oauth2/authorize/http/response/modes/iframerpc.js
@@ -1,5 +1,20 @@
 exports = module.exports = function() {
-  return require('oauth2orize-iframerpcrm');
+  return require('oauth2orize-iframerpcrm')(function(txn, cb) {
+    console.log('EXTEND IT');
+    
+    var params = {};
+    
+    // TODO: generate login hint
+    
+    var selector = txn.res.authContext && txn.res.authContext.sessionSelector;
+    if (selector) {
+      params.session_state = {
+        extraQueryParams: { ss: selector }
+      };
+    }
+    
+    return cb(null, params);
+  });
 };
 
 exports['@implements'] = 'http://i.authnomicon.org/oauth2/authorization/http/ResponseMode';
