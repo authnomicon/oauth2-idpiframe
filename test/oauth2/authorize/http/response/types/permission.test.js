3d41608414ed5f5e6687202930dccf11c32d3884
Jared Hanson <jaredhanson@gmail.com>
2021-12-09 18:32:04 -0800

    Add tests.

--- test/oauth2/authorize/http/response/types/permission.test.js
+++ test/oauth2/authorize/http/response/types/permission.test.js
@@ -48,4 +48,74 @@ describe('oauth2/authorize/http/response/types/permission', function() {
       .catch(done);
   }); // should create response type without response modes
   
+  describe('issue', function() {
+    var container = new Object();
+    container.components = sinon.stub()
+    container.components.withArgs('http://i.authnomicon.org/oauth2/authorization/http/ResponseMode').returns([]);
+    container.components.withArgs('http://i.authnomicon.org/oauth2/authorization/http/ResponseParameters').returns([]);
+    var lhs = new Object();
+    
+    var permissionSpy = sinon.stub();
+    var factory = $require('../../../../../../com/oauth2/authorize/http/response/types/permission', {
+      'oauth2orize-permission': {
+        grant: { permission: permissionSpy }
+      }
+    });
+    
+    var issue;
+    
+    beforeEach(function(done) {
+      lhs.issue = sinon.stub().yieldsAsync(null, 'AJMrCA...');
+      
+      //factory(lhs, logger, container)
+      factory(logger, container)
+        .then(function(type) {
+          issue = permissionSpy.getCall(0).args[1];
+          done();
+        })
+        .catch(done);
+    });
+    
+    it('should issue login hint', function(done) {
+      var client = {
+        id: 's6BhdRkqt3',
+        name: 'My Example Client'
+      };
+      var user = {
+        id: '248289761001',
+        displayName: 'Jane Doe'
+      };
+      var ares = {
+        allow: true
+      }
+      var areq = {
+        type: 'id_token',
+        clientID: 's6BhdRkqt3',
+        redirectURI: 'https://client.example.com/cb',
+        state: 'xyz'
+      }
+      
+      issue(client, user, ares, areq, {}, function(err, hint) {
+        if (err) { return done(err); }
+        
+        /*
+        expect(idts.issue.callCount).to.equal(1);
+        expect(idts.issue.getCall(0).args[0]).to.deep.equal({
+          user: {
+            id: '248289761001',
+            displayName: 'Jane Doe'
+          },
+          client: {
+            id: 's6BhdRkqt3',
+            name: 'My Example Client'
+          }
+        });
+        */
+        expect(hint).to.equal('A-LOGIN-HINT');
+        done();
+      });
+    }); // should issue login hint
+    
+  }); // issue
+  
 });
