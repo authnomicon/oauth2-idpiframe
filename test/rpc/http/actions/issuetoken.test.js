1e9ecdd3d3f2bead5e943a1dd15b3d87fc0e5c63
Jared Hanson <jaredhanson@gmail.com>
2021-12-10 16:51:48 -0800

    Prepping tests.

--- test/rpc/http/actions/issuetoken.test.js
+++ test/rpc/http/actions/issuetoken.test.js
@@ -14,4 +14,52 @@ describe('rpc/http/actions/issuetoken', function() {
     expect(factory['@singleton']).to.be.undefined;
   });
   
+  
+  function evaluate(req, res, next) {
+    res.json({ access_token: '2YotnFZFEjr1zCsicMWpAA', token_type: 'example' });
+  };
+  
+  var server = {
+    authorization: function(validate, immediate) {
+      return function(req, res, next) {
+        validate(req.query.client_id, req.query.redirect_uri, function(err, client, redirectURI, webOrigin) {
+          if (err) { return next(err); }
+          req.oauth2 = {
+            client: client,
+            redirectURI: redirectURI,
+            webOrigin: webOrigin
+          };
+        
+          immediate(req.oauth2, function(err, allow) {
+            if (err) { return next(err); }
+            if (allow) { return res.redirect(req.oauth2.redirectURI); }
+            return next();
+          })
+        })
+      };
+    },
+    
+    authorizationError: function() {
+      return function(err, req, res, next) {
+        next(err);
+      };
+    }
+  };
+  
+  function authenticate() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  function state() {
+    return function(req, res, next) {
+      next();
+    };
+  }
+  
+  describe('handler', function() {
+    
+  }); // handler
+  
 });
