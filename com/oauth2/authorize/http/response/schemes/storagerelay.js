e2eaf3c002ac47b5fbe0ed75bdae0f8ed64cc72b
Jared Hanson <jaredhanson@gmail.com>
2021-12-14 20:36:49 -0800

    Add doc link.

--- com/oauth2/authorize/http/response/schemes/storagerelay.js
+++ com/oauth2/authorize/http/response/schemes/storagerelay.js
@@ -1,6 +1,8 @@
 exports = module.exports = function() {
   var url = require('url');
   
+  // http://lists.openid.net/pipermail/openid-specs-ab/Week-of-Mon-20151116/005865.html
+  
   return {
     verify: function(client, redirectURI) {
       var uri = url.parse(redirectURI);
