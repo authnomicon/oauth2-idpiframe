2f246c8c83c6be074672b725f1ed7a6629ed02f7
Jared Hanson <jaredhanson@gmail.com>
2023-03-15 07:15:13 -0700

    Fix paths.

--- com/iframe/service.js
+++ com/iframe/service.js
@@ -4,8 +4,8 @@ exports = module.exports = function() {
   
   var router = new express.Router();
   //router.use(express.static(path.resolve(__dirname, 'public')));
-  router.use(express.static(path.resolve(__dirname, '../../../www')));
-  router.use('/js/lib', express.static(path.join(__dirname, '../../../www/node_modules')));
+  router.use(express.static(path.resolve(__dirname, '../../www')));
+  router.use('/js/lib', express.static(path.join(__dirname, '../../www/node_modules')));
   
   return router;
 };
