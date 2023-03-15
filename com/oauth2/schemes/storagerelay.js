95abbff58527b579b427d3ee78b5017eb3ad3152
Jared Hanson <jaredhanson@gmail.com>
2023-03-15 07:27:44 -0700

    Clean up.

--- com/oauth2/schemes/storagerelay.js
+++ com/oauth2/schemes/storagerelay.js
@@ -1,5 +1,7 @@
+// Module dependencies.
+var url = require('url');
+
 exports = module.exports = function() {
-  var url = require('url');
   
   // http://lists.openid.net/pipermail/openid-specs-ab/Week-of-Mon-20151116/005865.html
   
