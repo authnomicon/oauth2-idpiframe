e84dbf7782970b267a882464d8529f2324aa893f
Jared Hanson <jaredhanson@gmail.com>
2023-03-15 07:22:40 -0700

    Clean up.

--- com/iframe/service.js
+++ com/iframe/service.js
@@ -1,8 +1,9 @@
+// Module dependencies.
+var express = require('express');
+var path = require('path');
+
 exports = module.exports = function() {
-  var express = require('express');
-  var path = require('path');
-  
-  var router = new express.Router();
+  var router = express.Router();
   //router.use(express.static(path.resolve(__dirname, 'public')));
   router.use(express.static(path.resolve(__dirname, '../../www')));
   router.use('/js/lib', express.static(path.join(__dirname, '../../www/node_modules')));
