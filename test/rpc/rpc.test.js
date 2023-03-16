9533bd064560b05f3410672f43d4577c51b31c99
Jared Hanson <jaredhanson@gmail.com>
2023-03-16 11:11:43 -0700

    Update tests.

--- test/rpc/rpc.test.js
+++ test/rpc/rpc.test.js
@@ -1,28 +1,54 @@
 /* global describe, it */
 
 var expect = require('chai').expect;
+var chai = require('chai');
+var sinon = require('sinon');
 var factory = require('../../com/rpc/rpc');
-var actions = require('../../lib/iframerpc')
+var iframerpc = require('../../lib/iframerpc')
 
 
 describe('rpc/rpc', function() {
   
-  it('should be annotated', function() {
-    expect(factory['@singleton']).to.be.undefined;
-  });
-  
-  /*
   it('should construct router', function() {
-    var router = new Object();
-    
-    //function checkOrigin() {};
-    //function issueToken() {};
-    //function listSessions() {};
+    function checkOrigin() {};
+    function issueToken() {};
+    function listSessions() {};
     
     var router = factory(checkOrigin, issueToken, listSessions);
     
-    expect(router).to.be.an.instanceof(actions.Router);
+    expect(router).to.be.an.instanceof(iframerpc.Router);
   });
-  */
+  
+  describe('router', function() {
+    
+    it('should dispatch checkOrigin action', function(done) {
+      var checkOrigin = sinon.spy(function(req, res, next) {
+        res.json({ valid: true });
+      });
+      var issueToken = sinon.spy();
+      var listSessions = sinon.spy();
+    
+      var router = factory(checkOrigin, issueToken, listSessions);
+    
+      chai.express.use(router.handle.bind(router))
+        .request(function(req, res) {
+          req.query = {
+            action: 'checkOrigin'
+          };
+        })
+        .finish(function() {
+          expect(checkOrigin).to.have.been.calledOnce;
+          expect(issueToken).to.not.have.been.called;
+          expect(listSessions).to.not.have.been.calledOnce;
+          
+          expect(this).to.have.status(200);
+          expect(this).to.have.body({ valid: true });
+          done();
+        })
+        .listen();
+    }); // should dispatch checkOrigin action
+    
+  }); // router
+  
   
 });
