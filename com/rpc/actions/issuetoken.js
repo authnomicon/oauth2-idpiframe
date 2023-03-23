ef7322a8b8c61765288a49e1e3c18704322df52f
Jared Hanson <jaredhanson@gmail.com>
2023-03-23 10:28:28 -0700

    Clean up.

--- com/rpc/actions/issuetoken.js
+++ com/rpc/actions/issuetoken.js
@@ -11,36 +11,21 @@ exports = module.exports = function(service, evaluate, clients, server, authenti
   
   return [
     function(req, res, next) {
-      console.log('ISSUE TOKEN BODY');
-      console.log(req.query);
-      next();
-    },
-    ////state({ external: true }), // FIXME: is store argument to component needed?
-    require('flowstate')({ external: true, store: store }), // TODO: can this be removed
-    function(req, res, next) {
       // Inspired by https://expressjs.com/en/4x/api.html#req.xhr
       req.iframerpc = true;
       next();
     },
+    // TODO: authenticate by login_hint
+    // TODO: Select user based on login hint, or make sure its the authed user
     authenticator.authenticate([ 'session', 'anonymous' ], { multi: true }),
     server.authorization(
       function validateClient(areq, cb) {
-        console.log('IDP IFRAME VALIDATE CLIENT');
-        console.log(areq);
-        //console.log(redirectURI);
-      
-        // TODO: Implement this properly
-      
         clients.read(areq.clientID, function(err, client) {
-        //clients.read('1', function(err, client) {
-          console.log('READ CLIENT');
-          console.log(err);
-          console.log(client);
-        
           if (err) { return cb(err); }
           if (!client) {
             return cb(new oauth2orize.TokenError('The OAuth client was not found.', 'invalid_client'));
           }
+          
           if (!client.webOrigins || client.webOrigins.indexOf(areq.origin) == -1) {
             return cb(new oauth2orize.TokenError('Invalid client for this origin.', 'access_denied', null, 403));
           }
@@ -49,10 +34,6 @@ exports = module.exports = function(service, evaluate, clients, server, authenti
         });
       },
       function(txn, cb) {
-        console.log('IMMEDIATE MODE IFRAME!');
-        console.log(txn);
-        
-        
         /* Responses seen
 404
 {
@@ -68,12 +49,17 @@ result {error: 'immediate_failed', detail: 'Request could not be auto-approved.'
 rpcToken:  "cfjh_jzcXhhT7mLM"
         */
         
+        // TODO: Put this back
+        /*
+      if (!req.user) {
+        // matches Google's response, even though non-standard
+        return res.status(200).json({ error: 'USER_LOGGED_OUT', detail: 'No active session found.' });
+      }
+        */
+        
       
         var zreq = new aaa.Request(txn.client, txn.req, txn.user);
         service(zreq, function(err, zres) {
-          console.log(err);
-          console.log(zres);
-          
           if (err) { return cb(err); }
           
           if (zres.allow === true) {
@@ -99,36 +85,12 @@ rpcToken:  "cfjh_jzcXhhT7mLM"
             return cb(new iframerpc.IFrameRPCError('Request could not be auto-approved.', 'IMMEDIATE_FAILED', 200));
           }
         });
-      
-      
-      
-        //return cb(null, false);
       }
     ),
-    function(req, res, next) {
-      if (!req.user) {
-        // matches Google's response, even though non-standard
-        return res.status(200).json({ error: 'USER_LOGGED_OUT', detail: 'No active session found.' });
-      }
-      
-      // TODO: Select user based on login hint, or make sure its the authed user
-      
-      
-      console.log('OVERRIDE IDP IFRAME STUFF');
-      req.oauth2.req.responseMode = '.iframerpc';
-      req.oauth2.req.prompt = [ 'none' ];
-        
-      
-      next();
-    },
     function(err, req, res, next) {
       if (err.constructor.name !== 'IFrameRPCError') { return next(err); }
       return res.status(err.status || 500).json({ error: err.code, detail: err.message });
     }
-    
-    // TODO: Add error handling middleware here
-    // TODO: Check that this is right and not reloading the txn
-    //server.authorizationError()
   ];
 };
 
